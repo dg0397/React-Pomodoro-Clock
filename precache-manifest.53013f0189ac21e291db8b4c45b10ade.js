0fb959ea8ca14d98abe335d502f22abea8d1aad3
dg0397 <rgg0397@gmail.com>
2020-07-13 22:54:41 -0400

    Updates

---
+++ precache-manifest.53013f0189ac21e291db8b4c45b10ade.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde64e5ceacd6a4b191cac1003e13406",
+    "url": "/React-Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "2a1c8d12bdf4990a6b64",
+    "url": "/React-Pomodoro-Clock/static/js/2.488f0cd1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Pomodoro-Clock/static/js/2.488f0cd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89e0f5c29e367d58a9c3",
+    "url": "/React-Pomodoro-Clock/static/js/main.3354b6b0.chunk.js"
+  },
+  {
+    "revision": "b474ba065e608bcd7b8a",
+    "url": "/React-Pomodoro-Clock/static/js/runtime-main.967010ea.js"
+  }
+]);
